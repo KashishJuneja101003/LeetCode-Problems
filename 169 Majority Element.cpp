e0ebe1f6697c76a07a5d8eac7242fc8e56d77d96
Kashish Juneja <kashishjuneja1010@gmail.com>
2024-10-26 12:02:46 +0530

    Moore's Algo is used

--- 169 Majority Element.cpp
+++ 169 Majority Element.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // An element appearing more than (n/2) times, will always be in the middle
-        sort(nums.begin(), nums.end());
-        return nums[nums.size()/2];
+        // Moore's Algorithm
+        int freq=0, result=0;
+        for(int i=0; i<nums.size(); i++){
+            if(freq == 0) result = nums[i];
+
+            if(nums[i] == result) freq++;
+            else freq--;
+        }
+        return result;
     }
 };
