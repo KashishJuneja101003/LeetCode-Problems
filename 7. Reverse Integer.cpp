7586849459910567b98cccd5da405074190ebc07
Kashish Juneja <kashishjuneja1010@gmail.com>
2024-12-04 08:52:47 +0530

    Update 7. Reverse Integer.cpp

--- 7. Reverse Integer.cpp
+++ 7. Reverse Integer.cpp
@@ -3,9 +3,9 @@ public:
     int reverse(int x) {
         if (x <= INT_MIN || x >= INT_MAX) return 0;
 
-        long num = abs(x);
+        long num = x;
         long result = 0;
-        while(num > 0){
+        while(num != 0){
             int rem = num%10;
             num /= 10;
             result = (result)*10 + rem;
@@ -13,6 +13,6 @@ public:
 
         if (result <= INT_MIN || result >= INT_MAX) return 0;
 
-        return (x < 0) ? -1*result : result;
+        return result;
     }
 };
