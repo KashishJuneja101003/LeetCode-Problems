dd679685ce5219bcfb292017c665d8a10a72d01a
Kashish Juneja <kashishjuneja1010@gmail.com>
2024-12-31 11:59:17 +0530

    Update 39. Combination Sum.cpp

--- 39. Combination Sum.cpp
+++ 39. Combination Sum.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     set<vector<int>> s;
-    void helper(vector<int>& cand, int tar, vector<vector<int>>& ans, vector<int>& vec, int i){
-        if(i == cand.size() || 0 > tar){
+    void helper(vector<int>& cand, int tar, vector<vector<int>>& ans, int sum, vector<int>& vec, int i){
+        if(i == cand.size() || sum > tar){
             return;
         }
 
-        if(0 == tar){
+        if(sum == tar){
             if(s.find(vec) == s.end()){
                 ans.push_back({vec});
                 s.insert(vec);
@@ -14,23 +14,25 @@ public:
             return;
         }
 
+        sum += cand[i];
         vec.push_back(cand[i]);
 
         // Single Inclusion
-        helper(cand, tar-cand[i], ans, vec, i+1);
+        helper(cand, tar, ans, sum, vec, i+1);
 
         // Multiple Inclusion
-        helper(cand, tar-cand[i], ans, vec, i);
+        helper(cand, tar, ans, sum, vec, i);
 
         // Exclusion
+        sum -= cand[i]; // Backtracking Step
         vec.pop_back(); // Backtracking Step
-        helper(cand, tar, ans, vec, i+1);
+        helper(cand, tar, ans, sum, vec, i+1);
     }
 
     vector<vector<int>> combinationSum(vector<int>& cand, int tar) {
         vector<vector<int>> ans;
         vector<int> vec;
-        helper(cand, tar, ans, vec, 0);
+        helper(cand, tar, ans, 0, vec, 0);
         return ans;
     }
 };
