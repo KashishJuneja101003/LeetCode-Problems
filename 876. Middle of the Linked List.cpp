845e181b0475b8f3e620d1d46ac005c7e84bac7d
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-01-02 19:58:44 +0530

    Update 876. Middle of the Linked List.cpp

--- 876. Middle of the Linked List.cpp
+++ 876. Middle of the Linked List.cpp
@@ -11,18 +11,10 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        if(head == nullptr){
-            return head;
-        }
-
         ListNode *slow = head, *fast = head;
-        while(fast->next != nullptr && fast->next->next != nullptr){
+        while(fast != nullptr && fast->next != nullptr){
             slow = slow->next;
             fast = fast->next->next;
-        }
-
-        if(fast->next != nullptr){
-            slow = slow->next;
         }
 
         return slow;
