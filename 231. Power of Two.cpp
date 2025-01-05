812a0bbd9b93791d553257f27cabf577c5450d83
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-01-05 23:46:01 +0530

    Update 231. Power of Two.cpp
    Bit manipulation is used.
    Before: using loops

--- 231. Power of Two.cpp
+++ 231. Power of Two.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n == 1) return true; //2^0 = 1
-        if(n%2 != 0) return false;  // Odd number can't be power of 2
-        long int x = 2;
-        while(x < n){
-            x *= 2;
-        }
-        return (x == n) ? true:false;
+        if(n <= 0) return false;
+        return ((n & (n-1)) == 0);
     }
 };
