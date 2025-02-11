f7b150971281d6279661cdc13e7852cf52063d14
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-02-11 07:04:12 +0530

    Update 155. Min Stack.cpp

--- 155. Min Stack.cpp
+++ 155. Min Stack.cpp
@@ -1,3 +1,4 @@
+// Approach 1: Space: O(2*n)
 class MinStack {
     stack<pair<int, int>> s;
 public:
@@ -25,11 +26,42 @@ public:
     }
 };
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
+// Approach 2: Space: O(n)
+class MinStack {
+    stack<long long int> s;
+    long long int minVal;
+public:
+    MinStack() {
+    }
+    
+    void push(int val) {
+        if(s.empty()){
+            s.push(val);
+            minVal = val;
+        } else if(val < minVal){
+            s.push(2LL*val - minVal);
+            minVal = val;
+        } else{
+            s.push(val);
+        }
+    }
+    
+    void pop() {
+        if(s.top() < minVal){
+            minVal = 2*minVal - s.top();
+        }
+        s.pop();
+    }
+    
+    int top() {
+        if(s.top() < minVal){
+            return minVal;
+        } else{
+            return s.top();
+        }
+    }
+    
+    int getMin() {
+        return minVal;
+    }
+};
