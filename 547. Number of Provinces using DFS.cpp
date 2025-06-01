4cf2c88f3fda1c95045e30560a1a90b9538b66b5
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-06-01 16:49:50 +0530

    Update 547. Number of Provinces using DFS.cpp

--- 547. Number of Provinces using DFS.cpp
+++ 547. Number of Provinces using DFS.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    int V;
-    void DFS(vector<vector<int>>& adj, int u, vector<bool> &visited){
+    void DFS(vector<vector<int>>& adj, int u, vector<bool>& visited) {
         visited[u] = true;
 
-        for(int v = 0; v<V; v++){
-            if(!visited[v] && adj[u][v] == 1){
+        for (int v=0; v<adj.size(); v++) {
+            if (!visited[v] && adj[u][v] == 1) {
+                visited[v] = true;
                 DFS(adj, v, visited);
             }
         }
     }
 
     int findCircleNum(vector<vector<int>>& adj) {
-        V = adj.size();
+        int V = adj.size();
 
-        int countProvinces = 0;
         vector<bool> visited(V, false);
-        for(int i=0; i<V; i++){
-            if(!visited[i]){
+
+        int numProvinces = 0;
+        for (int i = 0; i < V; i++) {
+            // If a node is found that is unvisited, increment numProvinces and
+            // call DFS
+            if (!visited[i]) {
+                numProvinces++;
                 DFS(adj, i, visited);
-                countProvinces++;
             }
         }
-        return countProvinces;
+
+        return numProvinces;
     }
 };
