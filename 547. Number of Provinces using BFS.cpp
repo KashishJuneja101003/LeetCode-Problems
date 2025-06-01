5566f7570145ed1e127a987528d5acc61a9481e2
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-06-01 16:35:55 +0530

    Update 547. Number of Provinces using BFS.cpp

--- 547. Number of Provinces using BFS.cpp
+++ 547. Number of Provinces using BFS.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int V;
-    void BFS(vector<vector<int>>& adj, int u, vector<bool> &visited){
-        queue<int> que;
-        que.push(u);
+    void BFS(unordered_map<int, vector<int>> &adj, int u, vector<bool> &visited){
         visited[u] = true;
 
+        queue<int> que;
+        que.push(u);
+
         while(!que.empty()){
-            int u = que.front();
+            int node = que.front();
             que.pop();
 
-            for(int v=0; v<V; v++){
-                if(!visited[v] && adj[u][v] == 1){
+            for(int &v : adj[node]){
+                if(!visited[v]){
                     que.push(v);
                     visited[v] = true;
                 }
@@ -19,18 +19,35 @@ public:
         }
     }
 
-    int findCircleNum(vector<vector<int>>& adj) {
-        V = adj.size();
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        unordered_map<int, vector<int>> adj;
+        int V = isConnected.size();
 
-        int countProvinces = 0;
-        vector<bool> visited(V, false);
-       
+        // Creating adjacency list
         for(int i=0; i<V; i++){
-            if(!visited[i]){
-                BFS(adj, i, visited);
-                countProvinces++;
+            for(int j=0; j<V; j++){
+                if(i == j){
+                    continue;
+                }
+
+                if(isConnected[i][j] == 1) {
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
             }
         }
-        return countProvinces;
+
+        vector<bool> visited(V, false);
+
+        int numProvinces = 0;
+        for(int i=0; i<V; i++){
+            // If a node is found that is unvisited, increment numProvinces and call BFS 
+            if(!visited[i]){
+                numProvinces++;
+                BFS(adj, i, visited);
+            }
+        }
+
+        return numProvinces;
     }
 };
