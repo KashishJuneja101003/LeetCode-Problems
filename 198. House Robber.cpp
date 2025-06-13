56524cdf7ffc08c1472a5c15b029581494e37191
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-06-13 11:02:35 +0530

    Bottom up code added

--- 198. House Robber.cpp
+++ 198. House Robber.cpp
@@ -17,7 +17,7 @@ public:
 };
 */
 
-// Approach 2: Top Down (Recursion + Memoization) TC: O(n) SC: O(n)
+// Approach 2: Top Down (Recursion + Memoization) TC: O(n) SC: O(n) + Recursive stack
 class Solution {
 public:
     int solve(vector<int>& arr, int idx, int n, vector<int>& dp){
@@ -38,5 +38,26 @@ public:
         int n = nums.size();
         vector<int> dp(n, -1);
         return solve(nums, 0, n, dp);
+    }
+};
+
+
+// Approach 3: Bottom Up TC: O(n) SC: O(n)
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+
+        vector<int> maxAmt(n+1, 0);
+        maxAmt[0] = 0;
+        maxAmt[1] = nums[0];
+        
+        for(int i=2; i<=n; i++){
+            int steal = nums[i-1] + maxAmt[i-2];
+            int skip = maxAmt[i-1];
+            maxAmt[i] = max(steal, skip);
+        }
+        return maxAmt[n];
     }
 };
