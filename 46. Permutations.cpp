e835b08799011930453177083e0a17b0687c4a40
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-06-13 18:04:02 +0530

    Update 46. Permutations.cpp

--- 46. Permutations.cpp
+++ 46. Permutations.cpp
@@ -1,24 +1,58 @@
+// Backtracking: TC: O(n * n!) Total Space: O(n * n!) Auxiliary Space: O(n)
+/*
 class Solution {
 public:
-    void getPerms(vector<int>& nums, int idx, vector<vector<int>>& ans){
+    void getPermutations(vector<int>& temp, vector<int>& nums, unordered_set<int>& st, vector<vector<int>>& allPermutations){
+        if(temp.size() == nums.size()){
+            allPermutations.push_back(temp);
+            return;
+        }
+
+        for(int i=0; i<nums.size(); i++){
+            int el = nums[i];
+            if(st.find(el) == st.end()){
+                temp.push_back(el);
+                st.insert(el);
+                getPermutations(temp, nums, st, allPermutations);
+
+                // Backtracking Step
+                temp.pop_back();
+                st.erase(el);
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> temp;
+        vector<vector<int>> allPermutations;
+        unordered_set<int> st;
+
+        getPermutations(temp, nums, st, allPermutations);
+        return allPermutations;
+    }
+};
+*/
+
+// Approach 2: TC: O(n*n!) SC: O(n) of Recursion
+class Solution {
+public:
+    void getPermutations(vector<vector<int>>& allPermutations, vector<int>& nums, int idx){
         if(idx == nums.size()){
-            ans.push_back({nums});
+            allPermutations.push_back(nums);
             return;
         }
 
         for(int i=idx; i<nums.size(); i++){
-            swap(nums[idx], nums[i]); // idx => ith element choice
-            getPerms(nums, idx+1, ans);
+            swap(nums[i], nums[idx]);
+            getPermutations(allPermutations, nums, idx+1);
 
-            // Backtracking
-            swap(nums[idx], nums[i]);
+            // BT Step
+            swap(nums[i], nums[idx]);
         }
     }
-
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
-        getPerms(nums, 0, ans);
-
-        return ans;
+        vector<vector<int>> allPermutations;
+        
+        getPermutations(allPermutations, nums, 0);
+        return allPermutations;
     }
 };
