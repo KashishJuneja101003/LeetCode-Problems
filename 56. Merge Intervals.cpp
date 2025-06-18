a8a20c6551e6d2ef0de7e3ee0c14b9cab3eab43e
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-06-18 16:49:06 +0530

    Update 56. Merge Intervals.cpp

--- 56. Merge Intervals.cpp
+++ 56. Merge Intervals.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& arr) {
-        sort(arr.begin(), arr.end());
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
 
-        int n = arr.size();
-        
-        vector<vector<int>> mergedIntervals;
-        
-        int lowerBound = arr[0][0];
-        int upperBound = arr[0][1];
+        vector<vector<int>> nonOverlappingIntervals;
 
-        for(int i=1; i<n; i++){
-            int start = arr[i][0];
-            int end = arr[i][1];
+        int lowerBound = intervals[0][0];
+        int upperBound = intervals[0][1];
 
+        for(int i=1; i<intervals.size(); i++){
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+
+            // If the interval lies in current range, update lowerBound and upperBound
             if(start <= upperBound){
                 upperBound = max(upperBound, end);
-            } else{
-                mergedIntervals.push_back({lowerBound, upperBound});
+            } else {
+                nonOverlappingIntervals.push_back({lowerBound, upperBound});
                 lowerBound = start; upperBound = end;
             }
         }
-        mergedIntervals.push_back({lowerBound, upperBound});
+        
+        nonOverlappingIntervals.push_back({lowerBound, upperBound});
 
-        return mergedIntervals;
+        return nonOverlappingIntervals;
     }
 };
