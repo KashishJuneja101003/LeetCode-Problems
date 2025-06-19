f5eda5a016c8f86e4b38da0d71b5b4317cb8394f
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-06-19 18:47:44 +0530

    Updated with cleaner code

--- 763. Partition Labels.cpp
+++ 763. Partition Labels.cpp
@@ -1,39 +1,30 @@
-// Approach 1: Two Pointers TC: O(n) SC: O(26) => O(1)
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
         vector<int> ch_end(26, 0);
         int n = s.size();
 
-        for(int i=0; i<n; i++){
-            int c = s[i]-'a';
+        for (int i = 0; i < n; i++) {
+            int c = s[i] - 'a';
             ch_end[c] = i;
         }
 
-        // for(auto& it:ch_st_end){
-        //     cout<<it.first<<": "<<it.second.first<<", "<<it.second.second<<"\n";
-        // }
-
-        int i=0;
-        int l=0;
-        int r=0;
+        int i = 0;
+        int l = 0;
+        int r = 0;
 
         vector<int> result;
 
-        while(i < n){
-            if(l == r){
-                int c = s[l]-'a';
-                r = ch_end[c];
-            }
-            if(i == r){
-                result.push_back(r-l+1);
+        while (i < n) {
+            int end = ch_end[s[i] - 'a'];
+            r = max(r, end);
+            
+            if (i == r) {
+                result.push_back(r - l + 1);
                 l = r = ++i;
+                continue;
             }
-            if(i < r){
-                int end = ch_end[s[i]-'a'];
-                r = max(r, end);
-                i++;
-            }
+            i++;
         }
 
         return result;
