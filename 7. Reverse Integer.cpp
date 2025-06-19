a5e890495cf66bca452b71f927b932e0c096eebd
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-06-19 07:34:08 +0530

    Update 7. Reverse Integer.cpp

--- 7. Reverse Integer.cpp
+++ 7. Reverse Integer.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int reverse(int x) {
-        if (x <= INT_MIN || x >= INT_MAX) return 0;
-
-        long num = x;
-        long result = 0;
-        while(num != 0){
-            int rem = num%10;
-            num /= 10;
-            result = (result)*10 + rem;
+        int num = 0;
+        
+        while(x != 0){
+            int rem = x % 10;
+            x /= 10;
+            if((num > INT_MAX/10) || (num < INT_MIN/10)) return 0;
+            num = num*10 + rem;
         }
 
-        if (result <= INT_MIN || result >= INT_MAX) return 0;
-
-        return result;
+        return num;
     }
 };
