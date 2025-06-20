f30ae486c026ec8f11e833ac41785b757f1f520e
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-06-20 17:53:16 +0530

    Updated code with more cleaner and effective code
    TC: O(n+m+26)
    SC: O(26)
    Early exit

--- 242. Valid Anagram.cpp
+++ 242. Valid Anagram.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int s1[26] = {0}, t1[26] = {0};
+        vector<int> freq(26, 0);
+
         for(char c:s){
-            s1[c-'a'] += 1;
-        }
-        for(char c:t){
-            t1[c-'a'] += 1;
+            freq[c-'a']++;
         }
 
-        for(int i=0; i<26; i++){
-            if(s1[i] != t1[i]){
-                return false;
-            }
+        for(char c:t){
+            int idx = c-'a';
+            if(freq[idx] == 0) return false;
+            freq[idx]--;
+        }
+
+        for(int i:freq){
+            if(i > 0) return false;
         }
 
         return true;
