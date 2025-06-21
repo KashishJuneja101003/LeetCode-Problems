8d30d7c4d73313b30ba4ea987963ea4464daa7d4
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-06-21 06:44:32 +0530

    Update 704. Binary Search.cpp

--- 704. Binary Search.cpp
+++ 704. Binary Search.cpp
@@ -1,3 +1,19 @@
+// Approach 1: Linear Search: TC: O(n) SC: O(1)
+/*
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            if(nums[i] == target) return i;
+        }
+
+        return -1;
+    }
+};
+*/
+    
+// Approach 2: BS TC: O(log n) SC: O(1)
 class Solution {
 public:
     int search(vector<int>& arr, int target) {
