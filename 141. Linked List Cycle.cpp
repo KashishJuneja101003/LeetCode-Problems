ce7f9cbf7ed406cc4afa43aef8dae425c508e9ec
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-06-21 06:52:20 +0530

    Update 141. Linked List Cycle.cpp

--- 141. Linked List Cycle.cpp
+++ 141. Linked List Cycle.cpp
@@ -1,11 +1,26 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+// Approach 1: Brute Force TC: O(n) SC: O(n)
+/*
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == nullptr || head->next == nullptr) return false;
+        unordered_set<ListNode*> visited;
+        
+        ListNode* temp = head;
+
+        while(temp != nullptr){
+            if(visited.find(temp) != visited.end()){
+                return true;
+            }
+            visited.insert(temp);
+            temp = temp->next;
+        }
+        return false;
+    }
+};
+*/
+
+// Approach 2: Floyd's Cycle Detection TC: O(n) SC: O(1)
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
