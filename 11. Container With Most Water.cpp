969d508fc195fa8d7dac15b2fb92aa5af4f6a3c4
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-06-22 07:15:27 +0530

    Update 11. Container With Most Water.cpp

--- 11. Container With Most Water.cpp
+++ 11. Container With Most Water.cpp
@@ -1,18 +1,43 @@
+// Approach 1: Brute Force TC: O(n^2) SC: O(1)
+/*
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        // O(n) using 2 pointer approach
-      
-        int n = height.size(), maxVol = 0;
-        int i=0, j=n-1;
+        int n = height.size();
+        int maxArea = 0;
+        for(int i=0; i<n; i++){
+            int ht = height[i];
+            for(int j=i+1; j<n; j++){
+                int ht2 = height[j];
+                ht2 = (ht2 <= ht) ? ht2 : ht;
+                int area = ht2 * (j-i);
+                maxArea = max(maxArea, area);
+            }
+        }
+
+        return maxArea;
+    }
+};
+*/
+
+// Approach 2: Two Pointers TC: O(n) SC: O(1)
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int i = 0;
+        int j = n-1;
+        int maxArea = 0;
+        
         while(i < j){
-            int minHeight = min(height[i], height[j]);
-            int width = j-i;
-            maxVol = max(maxVol, minHeight*width);
-            
-            if (height[i] < height[j]) i++;
+            int h = min(height[i], height[j]);
+            int w = j-i;
+            int area = h*w;
+            maxArea = max(maxArea, area);
+            if(height[i] < height[j]) i++;
             else j--;
         }
-        return maxVol;
+
+        return maxArea;
     }
 };
