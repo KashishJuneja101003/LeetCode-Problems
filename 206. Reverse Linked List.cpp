1d6c1c17c034a804a1e33c2cbc7fe1fb0d455b39
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-06-25 16:09:03 +0530

    Added recursive approach

--- 206. Reverse Linked List.cpp
+++ 206. Reverse Linked List.cpp
@@ -1,31 +1,38 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// Approach 1: Iterative TC: O(n) SC: O(1)
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if(head == nullptr){
-            return nullptr;
-        }
+        if (head == nullptr || head->next == nullptr)
+            return head;
 
+        ListNode* curr = head;
+        ListNode* prev = nullptr;
+        ListNode* next = head;
 
-        ListNode *next, *curr = head, *prev = nullptr;
         while(curr != nullptr){
             next = curr->next;
             curr->next = prev;
             prev = curr;
             curr = next;
         }
-        
-        head = prev;
 
-        return head;
+        return prev;
     }
 };
+
+// Approach 2: Recursive TC: O(n) SC: O(n)
+/*
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr)
+            return head;
+
+        ListNode* reverseHead = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+
+        return reverseHead;
+    }
+};
+*/
