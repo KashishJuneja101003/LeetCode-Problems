0499b639251fbe43bfaead3a85f439fd823dd9b4
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-06-25 16:44:24 +0530

    Update 215. Kth Largest Element in an Array.cpp

--- 215. Kth Largest Element in an Array.cpp
+++ 215. Kth Largest Element in an Array.cpp
@@ -1,17 +1,23 @@
+// Approach 1: Sorting TC: O(n log n) SC: O(1)
+/*
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size()-k];
+    }
+};
+*/
+
+// Approach 2: Min-Heap TC: O(n log k) SC: O(k)
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         priority_queue<int, vector<int>, greater<int>> pq;
 
-        for(int i=0; i<k; i++){
-            pq.push(nums[i]);
-        }
-
-        for(int i=k; i<nums.size(); i++){
-            if(nums[i] > pq.top()){
-                pq.pop();
-                pq.push(nums[i]);
-            }
+        for(int i:nums){
+            pq.push(i);
+            if(pq.size() > k) pq.pop();
         }
 
         return pq.top();
