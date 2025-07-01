c9a9bbb2f90e90b6096c57dc4fa3d0bcc71322bc
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-07-01 20:55:44 +0530

    Update 88. Merge Sorted Arrays.cpp

--- 88. Merge Sorted Arrays.cpp
+++ 88. Merge Sorted Arrays.cpp
@@ -1,21 +1,25 @@
+// Approach 1: TC: O(m+n) SC: O(1)
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int idx = m+n-1, i = m-1, j = n-1;
+        int idx = m+n-1;
+        int i = m-1;
+        int j = n-1;
 
         while(i >= 0 && j >= 0){
-            if (nums1[i] >= nums2[j]){
-                nums1[idx] = nums1[i];
-                i--; idx--;
-            }
-            else{
+            if(nums2[j] >= nums1[i]){
                 nums1[idx] = nums2[j];
-                j--; idx--;
+                j--;
+            } else{
+                swap(nums1[i], nums1[idx]);
+                i--;
             }
+            idx--; 
         }
 
         while(j >= 0){
-            nums1[idx--] = nums2[j--];
+            nums1[idx] = nums2[j];
+            idx--; j--;
         }
     }
 };
