914b2fbe9828eb6800d293707ee7012c585b9845
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-07-02 16:15:02 +0530

    Update 53. Maximum Subarray.cpp

--- 53. Maximum Subarray.cpp
+++ 53. Maximum Subarray.cpp
@@ -1,13 +1,36 @@
+// Approach 1: Brute Force TC: O(n^2) SC: O(1)
+/*
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int currSum = 0, maxSum = INT_MIN;
-        // Kadane's Algorithm
-        for(int i=0; i<nums.size(); i++){
-            currSum += nums[i];
-            maxSum = max(maxSum, currSum);
-            if(currSum < 0) currSum = 0;
+        int maxSum = 0;
+        int n = nums.size();
+        
+        for(int i=0; i<n; i++){
+            int currSum = 0;
+            for(int j=i; j<n; j++){
+                currSum += nums[j];
+                maxSum = max(maxSum, currSum);
+            }
         }
+        return maxSum;
+    }
+};
+*/
+
+// Approach 2: Kadane's Algorithm TC: O(n) SC: O(1)
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = INT_MIN;
+        int sum = 0;
+
+        for(int i:nums){
+            sum += i;
+            maxSum = max(sum, maxSum);
+            if(sum < 0) sum = 0;
+        }
+
         return maxSum;
     }
 };
