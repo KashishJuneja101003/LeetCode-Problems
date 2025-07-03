bc2de581d95ff6d0705ae393416b89353a54cc87
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-07-03 20:11:07 +0530

    Update 225. Implement Stack using Queues.cpp

--- 225. Implement Stack using Queues.cpp
+++ 225. Implement Stack using Queues.cpp
@@ -1,7 +1,54 @@
+// Approach 1: Using 2 Queues push(x): O(1), pop(): O(n), top(): O(n), empty(): O(1)
 class MyStack {
 public:
     queue<int> q1;
-    MyStack() {   
+    queue<int> q2;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q1.push(x);
+    }
+    
+    int pop() {
+        while(q1.size () > 1){
+            q2.push(q1.front());
+            q1.pop();
+        }
+
+        int val = q1.front();
+        q1.pop();
+        swap(q1,q2);
+
+        return val;
+    }
+    
+    int top() {
+        while(q1.size () > 1){
+            q2.push(q1.front());
+            q1.pop();
+        }
+
+        int val = q1.front();
+        q2.push(val);
+        q1.pop();
+        swap(q1, q2);
+
+        return val;
+    }
+    
+    bool empty() {
+        return q1.size() == 0;
+    }
+};
+
+// Approach 2: Using 1 Queue push(x): O(n), pop(): O(1), top(): O(1), empty(): O(1)
+class MyStack {
+public:
+    queue<int> q1;
+    MyStack() {
+        
     }
     
     void push(int x) {
@@ -10,7 +57,7 @@ public:
         for(int i=0; i<q1.size()-1; i++){
             q1.push(q1.front());
             q1.pop();
-        }
+        }        
     }
     
     int pop() {
@@ -28,11 +75,30 @@ public:
     }
 };
 
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack* obj = new MyStack();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->top();
- * bool param_4 = obj->empty();
- */
+// Approach 3: Using Deque push(x): O(1), pop(): O(1), top(): O(1), empty(): O(1)
+// Not acceptable everywhere as deque offers control fron front end too
+class MyStack {
+public:
+    deque<int> dq;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        dq.push_back(x);
+    }
+    
+    int pop() {
+        int val = dq.back();
+        dq.pop_back();
+        return val;
+    }
+    
+    int top() {
+        return dq.back();
+    }
+    
+    bool empty() {
+        return dq.size() == 0;
+    }
+};
