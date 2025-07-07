eb73a9d2fb6ec518d92e260cdaf8b47040ba9f02
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-07-07 19:28:25 +0530

    Update 572. Subtree of Another Tree.cpp

--- 572. Subtree of Another Tree.cpp
+++ 572. Subtree of Another Tree.cpp
@@ -1,34 +1,17 @@
+// Approach 1: Recursive TC: O(n * m) SC: O(n + m) (due to recursion stack)
 class Solution {
 public:
-    bool isIdentical(TreeNode* root, TreeNode* subRoot){
-        if(root == nullptr && subRoot == nullptr){
-            return true;
-        }
+    bool isSame(TreeNode* r, TreeNode* s){
+        if(!r && !s) return true;
+        if(!r || !s) return false;
+        if(r->val != s->val) return false;
 
-        if(root == nullptr || subRoot == nullptr){
-            return false;
-        }
-
-        if(root->val == subRoot->val){
-            return isIdentical(root->left, subRoot->left) && isIdentical(root->right, subRoot->right);
-        }
-
-        return false;
+        return isSame(r->left, s->left) && isSame(r->right, s->right);
     }
+
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if(subRoot == nullptr){
-            return true;
-        }
-        if(root == nullptr){
-            return false;
-        }
-
-        if(root->val == subRoot->val){
-            if(isIdentical(root, subRoot)){
-                return true;
-            }
-        }
-
+        if(!root) return false;
+        if(isSame(root, subRoot)) return true;
         return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
     }
 };
