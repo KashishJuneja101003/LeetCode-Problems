be34e71b7f4282ba5473ada97ac5b1444c8233a3
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-07-16 20:08:22 +0530

    Update and rename LeetCode Contest Questions/3452. Sum of Good Numbers.cpp to 3452. Sum of Good Numbers.cpp

---
+++ 3452. Sum of Good Numbers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOfGoodNumbers(vector<int>& nums, int k) {
+        int sumGood = 0;
+        for(int i=0; i<nums.size(); i++){
+            bool isGood = true;
+
+            if(i-k >= 0 && nums[i] <= nums[i-k]) isGood = false;
+            if(i+k < nums.size() && nums[i] <= nums[i+k]) isGood = false;
+
+            if(isGood) sumGood += nums[i];
+        }
+
+        return sumGood;
+    }
+};©leetcode
