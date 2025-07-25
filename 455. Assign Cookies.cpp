53d8c378f2c8840c17db4a4928a612bf7e1384f5
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-07-25 20:04:36 +0530

    Update 455. Assign Cookies.cpp

--- 455. Assign Cookies.cpp
+++ 455. Assign Cookies.cpp
@@ -1,21 +1,25 @@
+// TC: O(n log n + m log m + min(n, m)) SC: O(1)
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
 
-        int i=0, j=0, n1=g.size(), n2=s.size();
+        int i = 0;
+        int j = 0;
 
-        int contentedChildren = 0;
-        while(i<n1 && j<n2){
-            if(g[i] <= s[j]){
-                contentedChildren++;
-                s[j] -= g[i];
-                i++;
-            } 
-            j++;
-            
+        int content = 0;
+
+        while(i < g.size() && j < s.size()){
+            // Increment the size of the cookie until the greed of a child can't be satisfied
+            if(s[j] < g[i]) {j++; continue;}
+
+            // Cookie can satisfy the greed
+            // Number of contented children incremented by 1
+            // Look for next child (i++) and try to assign next cookie (j++)
+            content++; i++; j++;
         }
-        return contentedChildren;
+
+        return content;
     }
 };
