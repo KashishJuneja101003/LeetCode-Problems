d6b588a6eb5fcef366c430626384904ace824d03
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-07-26 10:11:06 +0530

    Update 860. Lemonade Change.cpp

--- 860. Lemonade Change.cpp
+++ 860. Lemonade Change.cpp
@@ -1,23 +1,29 @@
+// TC: O(n) SC: O(1)
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int count5 = 0, count10 = 0;
+        int fives = 0;
+        int tens = 0;
+        int twenties = 0;
 
-        for (int bill : bills) {
-            if (bill == 5) {
-                count5++;
-            } else if (bill == 10) {
-                if (count5 == 0) return false;
-                count5--;
-                count10++;
-            } else {  // bill == 20
-                if (count10 > 0 && count5 > 0) {  
-                    count10--;  // Give one $10 bill
-                    count5--;   // Give one $5 bill
-                } else if (count5 >= 3) {  
-                    count5 -= 3;  // Give three $5 bills
-                } else {
-                    return false;  // Not enough change
+        for(int& amt:bills){
+            if(amt == 5) fives++;
+            else if(amt == 10){
+                tens++;
+                if(fives >= 1) fives--;
+                else return false;
+            }
+            else if(amt == 20){
+                twenties++;
+
+                if(fives >=1 && tens >= 1) {
+                    fives--; tens--;
+                }
+                else if(fives >= 3) {
+                    fives -=3;
+                }
+                else{
+                    return false;
                 }
             }
         }
