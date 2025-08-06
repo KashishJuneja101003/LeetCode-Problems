8584658d03b31ac80809a32cec11070d40b0eaf9
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-08-06 16:38:44 +0530

    Create 32. Longest Valid Parentheses.cpp

---
+++ 32. Longest Valid Parentheses.cpp
@@ -0,0 +1,36 @@
+// Two-Pointers TC: O(n) SC: O(1)
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.size();
+        int len = 0;
+        int open = 0;
+        int close = 0;
+
+        for(int i=0; i<n; i++){
+            if(s[i] == '(') open++;
+            else close++;
+
+            if(open == close) {
+                len = max(len, open*2);
+            } else if(close > open){
+                open = 0; close = 0;
+            }
+        }
+
+        open = 0; close = 0;
+
+        for(int i=n-1; i>=0; i--){
+            if(s[i] == '(') open++;
+            else close++;
+
+            if(open == close) {
+                len = max(len, open*2);
+            } else if(open > close){
+                open = 0; close = 0;
+            }
+        }
+
+        return len;
+    }
+};
