21619ba8417fc1970e8dec7127966f1266a5a689
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-08-07 10:05:34 +0530

    Update 3. Longest Substring Without Repeating Characters.cpp

--- 3. Longest Substring Without Repeating Characters.cpp
+++ 3. Longest Substring Without Repeating Characters.cpp
@@ -1,20 +1,58 @@
+// Brute Force TC: O(n^2) SC: O(k)
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int longestSubstring = 0;
-        int left = 0, right = 0;
-        unordered_set<char> characterSet;
+        int n = s.size();
 
-        while (right < s.size()) {
-            while (characterSet.find(s[right]) != characterSet.end()) {
-                characterSet.erase(s[left]);
-                left++;
+        if(n <= 1) return n;
+
+        int maxLen = 0;
+        
+        for(int i=0; i<n; i++){
+            unordered_set<char> st;
+            int len = 0;
+            for(int j=i; j<n; j++){
+                if(st.find(s[j]) == st.end()){
+                    st.insert(s[j]);
+                    len++;
+                } else {
+                    break;
+                }
             }
-            characterSet.insert(s[right]);
-            longestSubstring = max(longestSubstring, right - left + 1);
-            right++;
+
+            maxLen = max(maxLen, len);
         }
 
-        return longestSubstring;
+        return maxLen;
+    }
+};
+
+// Two Ponters TC: O(n) SC: O(k)
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+
+        if (n <= 1)
+            return n;
+
+        int maxLen = 0;
+        unordered_set<char> st;
+
+        int left = 0;
+        int right = 0;
+        while(right < n){
+            if (st.find(s[right]) == st.end()) {
+                st.insert(s[right]);
+                right++;
+            } else {
+                st.erase(s[left]);
+                left++;
+            }
+
+            maxLen = max(maxLen, right-left);
+        }
+
+        return maxLen;
     }
 };
