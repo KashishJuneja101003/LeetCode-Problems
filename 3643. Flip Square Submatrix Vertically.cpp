af3a29c3788026db00b50c2e4488328120c81f19
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-08-10 13:03:11 +0530

    Update 3643. Flip Square Submatrix Vertically.cpp

--- 3643. Flip Square Submatrix Vertically.cpp
+++ 3643. Flip Square Submatrix Vertically.cpp
@@ -1,20 +1,16 @@
-// TC: O(rows*cols), rows = (x+k-1)/2, cols = y, SC: O(1)
+// TC: O(k^2) SC: O(1)
 class Solution {
 public:
     vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, int k) {
-        int rows = grid.size();
-        int cols = grid[0].size();
-
         int r1 = x;
-        int r2 = x+k-1;
+        int r2 = x + k - 1;
 
-        while(r1 < r2){
-            int c1 = y;
-            for(int i=0; i<k; i++){
-                swap(grid[r1][c1], grid[r2][c1]);
-                c1++;
+        while (r1 < r2) {
+            for (int c = y; c < y+k; c++) {
+                swap(grid[r1][c], grid[r2][c]);
             }
-            r1++; r2--;
+            r1++;
+            r2--;
         }
 
         return grid;
