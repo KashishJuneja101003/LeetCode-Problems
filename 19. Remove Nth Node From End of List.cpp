eda847ce38952b7520a71e3ca55769c76f289bea
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-08-11 16:52:35 +0530

    Update 19. Remove Nth Node From End of List.cpp

--- 19. Remove Nth Node From End of List.cpp
+++ 19. Remove Nth Node From End of List.cpp
@@ -1,35 +1,56 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// Two Pass: TC: O(n) SC: O(1)
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* slow = head, *fast = head;
+        int N = 0;
+        ListNode* temp = head;
 
-        for(int i=0; i<n; i++){
-            fast = fast->next;
+        while (temp) {
+            N++;
+            temp = temp->next;
         }
 
-        if(fast == nullptr){
-            return head->next;
+        int k = N - n;
+
+        if (k == 0) {
+            temp = head;
+            head = head->next;
+            delete temp;
+            return head;
         }
 
-        while(fast->next != nullptr){
+        ListNode* prev = head;
+        for (int i = 1; i < k; i++)
+            prev = prev->next;
+
+        temp = prev->next;
+        prev->next = temp->next;
+        delete temp;
+
+        return head;
+    }
+};
+
+// One Pass: TC: O(n) SC: O(1)
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int N = 0;
+        ListNode* fast = head;
+
+        for(int i=0; i<n; i++) fast = fast->next;
+
+        if(!fast) return head->next;
+
+        ListNode* slow = head;
+
+        while(fast->next){
             slow = slow->next;
             fast = fast->next;
         }
 
-        ListNode* delNode = slow->next;
         slow->next = slow->next->next;
 
-        delete delNode;
         return head;
     }
 };
