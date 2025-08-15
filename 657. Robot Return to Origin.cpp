83652119dde1ebf7709581e99de5c841ada778bb
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-08-15 13:35:01 +0530

    Create 657. Robot Return to Origin.cpp

---
+++ 657. Robot Return to Origin.cpp
@@ -0,0 +1,17 @@
+// TC: O(n) SC: O(1)
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0;
+        int y = 0;
+
+        for(char& c:moves){
+            if(c == 'U') x--;
+            else if(c == 'D') x++;
+            else if(c == 'L') y--;
+            else y++;
+        }
+
+        return x == 0 && y == 0;
+    }
+};
