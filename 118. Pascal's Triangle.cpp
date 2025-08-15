7d6bf64b1a42e4c6e66e7326dbad45f946c19538
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-08-15 13:45:31 +0530

    Update 118. Pascal's Triangle.cpp

--- 118. Pascal's Triangle.cpp
+++ 118. Pascal's Triangle.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>>mat(numRows);
-        for(int i=0; i<numRows; i++){
-            mat[i].resize(i + 1, 1);
-            for(int j=1; j<i; j++){
-                mat[i][j] = mat[i-1][j-1] + mat[i-1][j];
+        if(numRows == 1) return {{1}};
+        if(numRows == 2) return {{1}, {1,1}};
+
+        vector<vector<int>> mat;
+        mat.push_back({1});
+        mat.push_back({1,1});
+
+        for(int i=2; i<numRows; i++){
+            vector<int> temp(i+1);
+            temp[0] = temp[i] = 1;
+            for(int j = 1; j < i; j++){
+                temp[j] = mat[i-1][j-1] + mat[i-1][j];
             }
+            mat.push_back(temp);
         }
 
         return mat;
