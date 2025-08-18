4d47c78ec9c31eb875a96655e611bcf1f5fe32b1
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-08-18 22:12:52 +0530

    Update 443. String Compression.cpp

--- 443. String Compression.cpp
+++ 443. String Compression.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int n = chars.size(), idx = 0;
-
+        int idx = 0;
+        int n = chars.size();
+        
         for(int i=0; i<n; ){
             char c = chars[i];
             int count = 0;
-
-            while(i < n && chars[i] == c){
-                count++; i++;
-            }
+            
+            while(i < n && chars[i] == c) {count++; i++;}
 
             chars[idx++] = c;
-
             if(count > 1){
                 string s = to_string(count);
-                
-                for(char ch:s){
-                    chars[idx] = ch;
-                    idx++;
+
+                for(char& ch:s){
+                    chars[idx++] = ch;
                 }
             }
-
         }
 
         return idx;
