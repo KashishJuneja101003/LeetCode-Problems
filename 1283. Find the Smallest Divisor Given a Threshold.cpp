cbc358d8003381aa3bf8888037421ac9c24b4644
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-09-16 16:50:25 +0530

    Update 1283. Find the Smallest Divisor Given a Threshold.cpp

--- 1283. Find the Smallest Divisor Given a Threshold.cpp
+++ 1283. Find the Smallest Divisor Given a Threshold.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-    int divisionSum(vector<int>& nums, int d){
+    bool isValid(vector<int>& nums, int threshold, int divisor) {
         int sum = 0;
 
-        for(int i:nums) {
-            sum += (ceil((double)i/d));
+        for (int i : nums) {
+            sum += ceil((i + divisor - 1 ) / divisor);
+
+            if (sum > threshold)
+                return false;
         }
-        return sum;
+
+        return sum <= threshold;
     }
 
     int smallestDivisor(vector<int>& nums, int threshold) {
-        int n = nums.size();
+        int left = 1;
+        int right = *max_element(nums.begin(), nums.end());
 
-        // Find maximum element in array and initialize end with that element
-        int start = 1, end = *max_element(nums.begin(), nums.end());
-        int smallestDivisor = -1;
+        int divisor = -1;
 
-        // Loop to find the smallest divisor
-        while(start <= end){
-            int mid = start + (end-start)/2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-            if(divisionSum(nums, mid) <= threshold){
-                smallestDivisor = mid;
-                end = mid-1;
-            } else{
-                start = mid+1;
+            if (isValid(nums, threshold, mid)) {
+                divisor = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
 
-        return smallestDivisor;
+        return divisor;
     }
 };
