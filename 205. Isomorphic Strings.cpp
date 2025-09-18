92e23099a9ae6feeb4de235ac2100d1073a4b12c
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-09-18 17:04:57 +0530

    Update 205. Isomorphic Strings.cpp

--- 205. Isomorphic Strings.cpp
+++ 205. Isomorphic Strings.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int n=s.length();
+        unordered_map<char, char> mp1, mp2;
 
-        int m1[256] = {0}, m2[250] = {0};
-        for(int i=0; i<n; i++){
-            if(m1[s[i]] != m2[t[i]]){
-                return false;
+        int n = s.size();
+
+        for (int i = 0; i < n; i++) {
+            char c1 = s[i];
+            char c2 = t[i];
+            if (mp1.find(c1) != mp1.end()) {
+                if (mp1[c1] != c2)
+                    return false;
+            }
+            if (mp2.find(c2) != mp2.end()) {
+                if (mp2[c2] != c1)
+                    return false;
             }
 
-            m1[s[i]] = i+1;
-            m2[t[i]] = i+1;
+            mp1[c1] = c2;
+            mp2[c2] = c1;
         }
 
         return true;
