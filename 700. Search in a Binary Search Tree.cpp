f3bfc4ccf651a4d3e40ce96c832625e23a458216
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-09-22 21:58:21 +0530

    Update 700. Search in a Binary Search Tree.cpp

--- 700. Search in a Binary Search Tree.cpp
+++ 700. Search in a Binary Search Tree.cpp
@@ -1,3 +1,4 @@
+// Iterative TC: O(h) SC: O(h)
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
@@ -5,5 +6,21 @@ public:
         if(val == root->val) return root;
         else if(val < root->val) return searchBST(root->left, val);
         else return searchBST(root->right, val);
+    }
+};
+
+// Iterative TC: O(h) SC: O(1)
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        TreeNode* node = root;
+
+        while(node){
+            if(node->val == val) return node;
+            else if (node->val < val) node = node->right;
+            else node = node->left;
+        }
+
+        return node;
     }
 };
