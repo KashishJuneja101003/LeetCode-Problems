4743920a1998f7ce014d3163fc468823cd2fdb51
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-10-13 10:14:29 +0530

    Create 2273. Find Resultant Array After Removing Anagrams.cpp

---
+++ 2273. Find Resultant Array After Removing Anagrams.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isAnagram(string& a, string& b) {
+        vector<int> freq(26, 0);
+
+        for (char& c : a) {
+            freq[c - 'a']++;
+        }
+        for (char& c : b) {
+            freq[c - 'a']--;
+        }
+
+        for (int& f : freq) {
+            if (f != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    vector<string> removeAnagrams(vector<string>& words) {
+        int n = words.size();
+        vector<string> ans;
+        int curr = 0;
+        ans.push_back(words[0]);
+
+        for (int i = 1; i < n; i++) {
+            if (!isAnagram(words[curr], words[i])) {
+                ans.push_back(words[i]);
+                curr = i;
+            }
+        }
+
+        return ans;
+    }
+};
